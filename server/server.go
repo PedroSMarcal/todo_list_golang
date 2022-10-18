8c953aee1a804f9aa6fb95969975ac315182cdaa
Pedro Henrique Silva Marçal <pedro.marcal@q2pay.com.br>
2022-10-18 18:48:12 -0300

    set routes

--- server/server.go
+++ server/server.go
@@ -9,6 +9,11 @@ import (
 )
 
 func setRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("/create", todo.CreateTask)
+	mux.HandleFunc("/update", todo.UpdateTask)
+	mux.HandleFunc("/finish", todo.Finish)
+	mux.HandleFunc("/get", todo.GetTask)
+
 	mux.HandleFunc("/health", todo.HealthCheck)
 }
 
